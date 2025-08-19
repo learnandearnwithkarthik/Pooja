8b0506fdcd66a078a6ee095f5c8454dc8ec0ee35
learnandearnwithkarthik <125831794+learnandearnwithkarthik@users.noreply.github.com>
2025-08-20 04:39:54 +0530

    Update axiosInstance.js

--- backend/utils/axiosInstance.js
+++ backend/utils/axiosInstance.js
@@ -4,7 +4,6 @@ import { BASE_URL } from "./constants";
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
     timeout: 10000,
-    // withCredentials: true, // ✅ Ensure credentials are included
     headers: {
         "Content-Type": "application/json",
     },
@@ -12,21 +11,16 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
     (config) => {
-        const accessToken = localStorage.getItem("token");  // Get token from localStorage
-//        console.log("Access Token:", accessToken);  // Debugging
-
-        if (accessToken) {
+        const token = localStorage.getItem("token");
+        if (token) {
             config.headers = {
                 ...config.headers,
-                authorization: `Bearer ${accessToken}`,  // ✅ Add token here
+                Authorization: `Bearer ${token}`,
             };
         }
-
-//        console.log("Request Headers:", config.headers);  // Debugging
         return config;
     },
     (error) => Promise.reject(error)
 );
-
 
 export default axiosInstance;
